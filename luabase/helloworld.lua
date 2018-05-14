ed3fb6ac280f47747f3b5065ba63a6fa1d294d41
qiyamiky@qq.com <NICKY>
2018-05-14 17:55:22 +0800

    test env

---
+++ luabase/helloworld.lua
@@ -0,0 +1,9 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: Administrator
+-- Date: 2018-05-14
+-- Time: 下午 05:29
+-- To change this template use File | Settings | File Templates.
+--
+
+print("Hello World")
